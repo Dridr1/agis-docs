2fd25baf094ba7c28a9d95a4ee07e88f4abb77ed
Dridr1 <a.driano@outlook.com>
2024-05-15 00:37:06 -0300

    release: first build

---
+++ build/assets/js/aba21aa0.37c5c0a6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkagis=self.webpackChunkagis||[]).push([[742],{7093:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
---
+++ build/assets/js/acecf23e.fc16dd52.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkagis=self.webpackChunkagis||[]).push([[903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog"}')}}]);
