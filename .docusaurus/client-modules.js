85090ad4a80f3008c6acea4014b9f109967f7c23
Dridr1 <a.driano@outlook.com>
2024-05-15 00:46:10 -0300

    chore: preparando deploy no gh-pages

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/adrianof/Documents/unb/UnB/agis/node_modules/infima/dist/css/default/default.css"),
-  require("/home/adrianof/Documents/unb/UnB/agis/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/adrianof/Documents/unb/UnB/agis/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/adrianof/Documents/unb/UnB/agis/src/css/custom.css"),
-];
