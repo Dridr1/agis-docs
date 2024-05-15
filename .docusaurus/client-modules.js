a8b66ec95392c26f9bcd5c73429096d7eeda1c1c
Dridr1 <a.driano@outlook.com>
2024-05-15 00:30:10 -0300

    initial commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/adrianof/Documents/unb/UnB/agis/node_modules/infima/dist/css/default/default.css"),
+  require("/home/adrianof/Documents/unb/UnB/agis/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/adrianof/Documents/unb/UnB/agis/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/adrianof/Documents/unb/UnB/agis/src/css/custom.css"),
+];
